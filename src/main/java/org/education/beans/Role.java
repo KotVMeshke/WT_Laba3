616c8224361e9f129f650fbcad1b437e676d53f2
KotVMeshke <dimon2003mail.ru37@gmail.com>
2023-12-10 20:18:32 +0300

    Create Project

---
+++ src/main/java/org/education/beans/Role.java
@@ -0,0 +1,16 @@
+package org.education.beans;
+
+/**
+ * @author nstah
+ * @version 0.5
+ * @brief Роли пользователей
+ */
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER,
+    ADMINISTRATOR,
+
+    User,
+    ROLE_NOT_AUTH,
+    ROLE_BANNED
+}
